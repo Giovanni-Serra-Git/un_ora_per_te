0472796e4dba10f872e2f7017731b53bcec241ef
Giovanni-Serra-Git <giovanni.serra.web@gmail.com>
2024-07-05 16:14:47 +0200

    changed width of itemGallery to 60%

--- src/components/homepage/itemGallery.jsx
+++ src/components/homepage/itemGallery.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
 function ItemGallery({src,title}) {
     return (
-        <div className="bg-pink-300 flex flex-col w-[72%] rounded-[10%]  md:w-[45%] lg:w-[22%]">
-        <div className="bg-pink-300 flex flex-col w-[72%] rounded-[10%] md:w-[45%] lg:w-[22%]">
-            <img className="flex-none md:w-full md:object-cover" src={src} alt={"Centro Estetico - Un ' ora per te " + title} />
+        <div className="bg-pink-300 flex flex-col w-[60%] rounded-[10%]  md:w-[45%] lg:w-[22%]">
+        <div className="bg-pink-300 flex flex-col rounded-[10%] md:w-[45%] lg:w-[22%]">
+            <img className="w-full flex-none md:w-full md:object-cover" src={src} alt={"Centro Estetico - Un ' ora per te " + title} />
             <div className="flex flex-col flex-grow items-center justify-center">
                 <p className="title-gallery-homepage pt-3 pb-3 lg:pt-1 lg:pb-1">{title}</p>
             </div>
